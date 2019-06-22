4c67d7487012198de7fddb498b265924df570b29
Ashanti Williams <ashanti.a.williams@hotmail.com>
2019-06-22 13:39:35 -0600

    Updated ViewFinder App - Complete Iteration 1

---
+++ My Life SOOC/NewPhotoViewController.swift
@@ -0,0 +1,66 @@
+//
+//  NewPhotoViewController.swift
+//  My Life SOOC
+//
+//  Created by Ashanti Williams on 6/21/19.
+//  Copyright © 2019 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class NewPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+
+    
+    @IBOutlet var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    //Save Button Action Function
+    
+   //Photo Library Action Function
+    
+    @IBAction func openPhotoLibraryButtonTapped(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //Camera Action Function
+    @IBAction func openCameraButtonTapped(_ sender: UIButton) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+   
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
